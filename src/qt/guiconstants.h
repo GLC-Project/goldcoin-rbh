11ceaa85cda906d57193a577796d4900f5abe03e
Peter Bushnell <bushsolo@gmail.com>
2019-02-04 13:52:56 +0000

    Rename

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -46,10 +46,10 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
-#define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
+#define QAPP_ORG_NAME "Goldcoin"
+#define QAPP_ORG_DOMAIN "goldcoin.org"
+#define QAPP_APP_NAME_DEFAULT "Goldcoin-Qt"
+#define QAPP_APP_NAME_TESTNET "Goldcoin-Qt-testnet"
+#define QAPP_APP_NAME_REGTEST "Goldcoin-Qt-regtest"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
