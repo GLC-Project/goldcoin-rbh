bdfcd0ff67e780b36ce61d881f6827eb6cf99815
Peter Bushnell <bushsolo@gmail.com>
2019-02-07 10:38:00 +0000

    GoldenRiver combine loops

--- src/pow.cpp
+++ src/pow.cpp
@@ -80,7 +80,7 @@ unsigned int GoldenRiver(const CBlockIndex* pindexLast, const Consensus::Params&
     if ((pindexLast->nHeight + 1) != nInterval)
         blockstogoback = nInterval;
     const CBlockIndex* pindexFirst = pindexLast;
-    for (int i = 0; pindexFirst && i < blockstogoback; i++)
+    for (int i = 0; pindexFirst && i < blockstogoback; ++i)
         pindexFirst = pindexFirst->pprev;
     assert(pindexFirst);
 
@@ -88,81 +88,55 @@ unsigned int GoldenRiver(const CBlockIndex* pindexLast, const Consensus::Params&
     //First we find the last 60 blocks and take the time between blocks
     //That gives us a list of 59 time differences
     //Then we take the median of those times and multiply it by 60 to get our actualtimespan
+    // We want to limit the possible difficulty raise/fall over 60 and 240 blocks here
+    // So we get the difficulty at 60 and 240 blocks ago
     CBlockIndex tblock1 = *pindexLast;//We want to copy pindexLast to avoid changing it accidentally
     CBlockIndex* tblock2 = &tblock1;
     std::vector<int64_t> last60BlockTimes;
-    while (last60BlockTimes.size() < 60)
+    std::vector<int64_t> last120BlockTimes;
+    int64_t nbits60ago = 0ULL;
+    int64_t nbits240ago = 0ULL;
+    int counter = 0;
+    while (counter <= 240)
     {
-        last60BlockTimes.push_back(tblock2->GetBlockTime());
+        if (counter == 60)
+            nbits60ago = tblock2->nBits;
+
+        if (counter == 240)
+            nbits240ago = tblock2->nBits;
+
+        if (last60BlockTimes.size() < 60)
+            last60BlockTimes.push_back(tblock2->GetBlockTime());
+
+        if ((last120BlockTimes.size() < 120))
+            last120BlockTimes.push_back(tblock2->GetBlockTime());
 
         if (tblock2->pprev)   //should always be so
-        {
             tblock2 = tblock2->pprev;
-        }
+
+        ++counter;
     }
 
     std::vector<int64_t> last59TimeDifferences;
-
+    std::vector<int64_t> last119TimeDifferences;
+    int64_t total = 0;
     int xy = 0;
 
-    while (last59TimeDifferences.size() != 59)
+    while (xy < 119)
     {
-        if (xy == 59)
-        {
-            LogPrint(BCLog::DIFFICULTY, "This shouldn't have happened\n");
-            break;
-        }
-
-        last59TimeDifferences.push_back(llabs(last60BlockTimes[xy] - last60BlockTimes[xy + 1]));
-        xy++;
-    }
-    sort(last59TimeDifferences.begin(), last59TimeDifferences.end(), comp64);
-
-    LogPrint(BCLog::DIFFICULTY, "Median Time between blocks is: %d \n", last59TimeDifferences[29]);
-    int64_t nActualTimespan = llabs((last59TimeDifferences[29]));
-    int64_t medTime = nActualTimespan;
-
-    //We need to set this in a way that reflects how fast blocks are actually being solved..
-    //First we find the last 120 blocks and take the time between blocks
-    //That gives us a list of 119 time differences
-    //Then we take the average of those times and multiply it by 60 to get our actualtimespan
-    tblock1 = *pindexLast; //We want to copy pindexLast to avoid changing it accidentally
-    tblock2 = &tblock1;
-    std::vector<int64_t> last120BlockTimes;
-    while (last120BlockTimes.size() < 120)
-    {
-        last120BlockTimes.push_back(tblock2->GetBlockTime());
-
-        if (tblock2->pprev)   //should always be so
-        {
-            tblock2 = tblock2->pprev;
-        }
-    }
-
-    std::vector<int64_t> last119TimeDifferences;
-
-    xy = 0;
-
-    while (last119TimeDifferences.size() != 119)
-    {
-        if (xy == 119)
-        {
-            LogPrint(BCLog::DIFFICULTY, "This shouldn't have happened 2 \n");
-            break;
-        }
+        if (xy < 59)
+            last59TimeDifferences.push_back(llabs(last60BlockTimes[xy] - last60BlockTimes[xy + 1]));
 
         last119TimeDifferences.push_back(llabs(last120BlockTimes[xy] - last120BlockTimes[xy + 1]));
-        xy++;
+        total += last119TimeDifferences[xy];
+
+        ++xy;
     }
+    sort(last59TimeDifferences.begin(), last59TimeDifferences.end(), comp64);
+    LogPrint(BCLog::DIFFICULTY, "Median Time between blocks is: %d \n", last59TimeDifferences[29]);
 
-    int64_t total = 0;
-
-    for (int x = 0; x < 119; x++)
-    {
-        int64_t timeN = last119TimeDifferences[x];
-        total += timeN;
-    }
-
+    int64_t nActualTimespan = llabs((last59TimeDifferences[29]));
+    int64_t medTime = nActualTimespan;
     averageTime = total / 119;
 
     LogPrint(BCLog::DIFFICULTY, "Average time between blocks: %d\n", averageTime);
@@ -177,7 +151,6 @@ unsigned int GoldenRiver(const CBlockIndex* pindexLast, const Consensus::Params&
 
     //Fixes an issue where median time between blocks is greater than 120 seconds and is not permitted to be lower by the defence system
     //Causing difficulty to drop without end
-
     if (medTime >= 120)
     {
         //Check to see whether we are in a deadlock situation with the 51% defense system
@@ -188,10 +161,10 @@ unsigned int GoldenRiver(const CBlockIndex* pindexLast, const Consensus::Params&
         {
             if (llabs(last60BlockTimes.at(last60BlockTimes.size() - index) - last60BlockTimes.at(last60BlockTimes.size() - (index + 5))) == 600)
             {
-                numTooClose++;
+                ++numTooClose;
             }
 
-            index++;
+            ++index;
         }
 
         if (numTooClose > 0)
@@ -238,36 +211,6 @@ unsigned int GoldenRiver(const CBlockIndex* pindexLast, const Consensus::Params&
 
     nActualTimespan = medTime * 60;
 
-    tblock1 = *pindexLast;//We want to copy pindexLast to avoid changing it accidentally
-    tblock2 = &tblock1;
-
-    // We want to limit the possible difficulty raise/fall over 60 and 240 blocks here
-    // So we get the difficulty at 60 and 240 blocks ago
-    int64_t nbits60ago = 0ULL;
-    int64_t nbits240ago = 0ULL;
-    int counter = 0;
-
-    //Note: 0 is the current block, we want 60 past current
-    while (counter <= 240)
-    {
-        if (counter == 60)
-        {
-            nbits60ago = tblock2->nBits;
-
-        }
-        else if (counter == 240)
-        {
-            nbits240ago = tblock2->nBits;
-        }
-
-        if (tblock2->pprev)   //should always be so
-        {
-            tblock2 = tblock2->pprev;
-        }
-
-        counter++;
-    }
-
     //Now we get the old targets
     arith_uint256 bn60ago = 0, bn240ago = 0, bnLast = 0;
     bn60ago.SetCompact(nbits60ago);
@@ -287,9 +230,7 @@ unsigned int GoldenRiver(const CBlockIndex* pindexLast, const Consensus::Params&
     bnLast /= 8;
 
     if (!didHalfAdjust && bnNew > bnLast)
-    {
         bnNew.SetCompact(bnLast.GetCompact());
-    }
 
     // Set ceilings on difficulty increases per block
     //1.0/1.02 == 100/102
@@ -297,24 +238,18 @@ unsigned int GoldenRiver(const CBlockIndex* pindexLast, const Consensus::Params&
     bn60ago /= 102;
 
     if (bnNew < bn60ago)
-    {
         bnNew.SetCompact(bn60ago.GetCompact());
-    }
 
     //1.0/(1.02*4) ==  100 / 408
     bn240ago *= 100;
     bn240ago /= 408;
 
     if (bnNew < bn240ago)
-    {
         bnNew.SetCompact(bn240ago.GetCompact());
-    }
 
     //Sets a ceiling on highest target value (lowest possible difficulty)
     if (bnNew > bnProofOfWorkLimit)
-    {
         bnNew = bnProofOfWorkLimit;
-    }
 
     return bnNew.GetCompact();
 }
