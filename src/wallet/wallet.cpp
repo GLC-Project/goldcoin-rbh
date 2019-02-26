af1680824d69a619d9f9981655eb4a7812f22bc9
Peter Bushnell <bushsolo@gmail.com>
2019-02-26 14:40:21 +0000

    SignTransaction with SIGHASH_FORKID

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2609,7 +2609,7 @@ bool CWallet::SignTransaction(CMutableTransaction &tx)
         const CScript& scriptPubKey = mi->second.tx->vout[input.prevout.n].scriptPubKey;
         const CAmount& amount = mi->second.tx->vout[input.prevout.n].nValue;
         SignatureData sigdata;
-        if (!ProduceSignature(*this, MutableTransactionSignatureCreator(&tx, nIn, amount, SIGHASH_ALL), scriptPubKey, sigdata)) {
+        if (!ProduceSignature(*this, MutableTransactionSignatureCreator(&tx, nIn, amount, SIGHASH_ALL | SIGHASH_FORKID), scriptPubKey, sigdata)) {
             return false;
         }
         UpdateInput(input, sigdata);
